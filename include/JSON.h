909d4e976cd12073e2b2731f5d53012306dc1cc6
Jacob Smith <jacobsmithsinbox@gmail.com>
2022-01-08 00:00:43 -0700

    Update JSON.h

--- include/JSON.h
+++ include/JSON.h
@@ -1,36 +1,44 @@
-#pragma once	
+#pragma once
 
-#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// Enumeration for the types of JSON objects.
-enum JSONValue_e
+// Enumerations
+
+// Enumeration for the types of JSON objects
+enum JSONValueType_e
 {
-	JSONobject    = 0, // Anything within a "{ ... }".
-	JSONarray     = 1, // Any array.
-	JSONstring    = 2, // Any string.
-	JSONprimative = 3  // Any other value.
+    JSONobject    = 0, // Anything within a "{ ... }"
+    JSONarray     = 1, // Any array
+    JSONstring    = 2, // Any string
+    JSONprimative = 3  // Any other value
 };
+
+// Unions and structs
 
 // This union is used to differentiate pointers form double pointers, 
-// or more simply, arrays from non arrays.
+// or more simply, arrays from non arrays
 union JSONContent_u {
-	void*  nWhere;       // Pointer to object.
-	void** aWhere;       // Double pointer array of objects.
+    void  *n_where;       // Pointer to object
+    void **a_where;       // Double pointer array of objects
 };
-typedef union JSONContent_u JSONContent_t;
 
-// Container for the value
-struct JSONValue_s
+// Container for the JSON key/value pair
+struct JSONToken_s
 {
-	char*            name;    // The key.
-	enum JSONValue_e type;    // The type.
-	JSONContent_t    content; // Where the value appears, if type is array, then where is double pointer to text in array.
+    char                 *key;   // The key
+    union JSONContent_u   value; // The token
+    enum JSONValueType_e  type;  // The type.
 };
-typedef struct JSONValue_s JSONValue_t;
 
-int parseJSON ( char* text, size_t len, size_t count, JSONValue_t* where );   // Parses a JSON file and places tokens at an array of JSONValue_t* tokens. 
-                                                                              // If the function is called when the where parameter is nullptr, the
-                                                                              // function will return the number of entities that need to be allocated for.
+// Type definitions
+typedef union  JSONContent_u   JSONContent_t;
+typedef enum   JSONValueType_e JSONValueType_t;
+typedef struct JSONToken_s     JSONToken_t;
+
+// Function definitions
+int parse_json ( char *token_text, size_t len, size_t count, JSONToken_t *tokens ); // ✅ Parses a JSON file and places tokens at an array of JSONToken_t* tokens. 
+                                                                                    //     If the function is called when the tokens parameter is nullptr, the
+                                                                                    //     function will return the number of tokens that need to be allocated for.
